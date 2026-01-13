a394ffc73ed656f156910a25c715a4f4c8866199
Sebastian Berm <sebastian@sebsoft.nl>
2026-01-13 20:54:49 +0100

    Fix: Trust all proxies to ensure correct HTTPS URL generation

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(append: [
             \App\Http\Middleware\SetLocale::class,
         ]);
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
